8fd4990b635c29dd6f8557a954e57fe7c6d8111c
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2023-03-27 08:33:55 +0200

    creazione progetto

---
+++ mensj_InfoTest/mensj_Canditato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mensj_InfoTest
+{
+    abstract class mensj_Canditato
+    {
+       
+    }
+}

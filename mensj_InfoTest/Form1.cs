7458b5b842a77733577fce0210d51b42ee0000ee
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2023-03-27 10:56:58 +0200

    task 5

--- mensj_InfoTest/Form1.cs
+++ mensj_InfoTest/Form1.cs
@@ -12,9 +12,67 @@ namespace mensj_InfoTest
 {
     public partial class Form1 : Form
     {
+        public List<mensj_Canditato> lista;
         public Form1()
         {
             InitializeComponent();
+            lista = new List<mensj_Canditato>();
+        }
+        public void Aggiungi(mensj_Canditato c)
+        {
+            if(c!=null)
+                lista.Add(c);
+        }
+        public string[] Visualizzazione()
+        {
+            string[] righe = {};
+            for(int i = 0; i < lista.Count; i++)
+                righe[i] = lista[i].ToString();
+            return righe;
+        }
+        public string Modifica(mensj_Canditato n,int matricolaV)
+        {
+            if (n != null)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (lista[i].Matricola == matricolaV)
+                    { 
+                        n.Matricola = matricolaV;
+                        lista[i] = n;
+                        return "Candidato modificato";
+                    }
+                }
+                return "Candidato non trovato";
+            }
+            else
+                throw new Exception("il candidato non esiste");
+        }
+        public string Elimina(int matricola)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == matricola)
+                {
+                    lista.Remove(lista[i]);
+                    return "Candidato eliminato";
+                }    
+            }
+            return "Candidato non trovato";
+        }
+        public string[] VisuaIdonei()
+        {
+            string[] righe = { };
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].isIdoneo()==true)
+                    righe[i] = lista[i].ToString();
+            }
+            return righe;
+        }
+        public void sortD()
+        {
+            lista.Sort();
         }
     }
 }

b4b5b0a36a2ed176c8af0c024ce3d344a1001418
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2023-03-27 08:34:42 +0200

    creazione costruttori e metodi

--- mensj_InfoTest/mensj_Canditato.cs
+++ mensj_InfoTest/mensj_Canditato.cs
@@ -8,6 +8,47 @@ namespace mensj_InfoTest
 {
     abstract class mensj_Canditato
     {
-       
+        private int mensj_matricola;
+        private string mensj_nome;
+
+        public int Matricola
+        {
+            set { mensj_matricola = value; }
+            get { return mensj_matricola; }
+        }
+        public string Nome
+        {
+            set { mensj_nome = value; }
+            get { return mensj_nome; }
+        }
+        public mensj_Canditato()
+        {
+            Matricola = 0;
+            Nome = "N/A";
+        }
+        public mensj_Canditato(int m)
+        {
+            Matricola = m;
+            Nome = "N/A";
+        }
+        public mensj_Canditato(string n)
+        {
+            Nome = n;
+            Matricola = 0;
+        }
+        public mensj_Canditato(int m, string n)
+        {
+            Matricola = m;
+            Nome = n;
+        }
+        public bool isIdoneo()
+        {
+            return true;
+        }
+        public int punteggio()
+        {
+            return 0;
+        }
+
     }
 }
